1ce461e1de6c8c331e47e1feba21127dec9ecf4b
Ian Rose <ian.r.rose@gmail.com>
2017-04-30 17:15:57 -0700

    Remove relative rotation vector from reference frames figure.

--- figures/reference_frames.py
+++ figures/reference_frames.py
@@ -109,10 +109,6 @@ E = np.array([ [L1, 0, 0], [0, L2, 0], [0, 0, L3] ])
 theta = 30. * np.pi/180.
 phi = 0. * np.pi/180.
 omega = np.array([ np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)])
-#relative motion of the two frames
-alpha = 50. * np.pi/180.
-beta = 110. * np.pi/180.
-psi = np.array([ np.sin(alpha)*np.cos(beta), np.sin(alpha)*np.sin(beta), np.cos(alpha)])
 # tpw vector
 omega_dot = np.dot(E, omega.T) - np.dot(omega, np.dot(E, omega.T))*omega.T
 #omega_dot = 0.3*omega_dot/norm(omega_dot)
@@ -153,28 +149,6 @@ span = np.linspace(0, 1., 100)
 guideline = np.array([np.cos(phi)*np.sin(theta)*np.ones_like(span), np.sin(phi)*np.sin(theta)*np.ones_like(span), np.cos(theta)*span])
 ax.plot( guideline[0,:], guideline[1,:], guideline[2,:], 'k--', alpha=alpha_val)
 
-# plot relative rotation
-psi_arrow = Arrow3D([o[0], psi[0]], [o[1], psi[1]], [o[2], psi[2]], **arrow_prop_dict)
-ax.add_artist(psi_arrow)
-plot_rotation_arrow(ax, psi_arrow, ccw=False)
-# add helper lines for psi
-span = np.linspace(0, 1., 100)
-guideline = np.array([np.cos(beta)*np.sin(alpha)*np.ones_like(span), np.sin(beta)*np.sin(alpha)*np.ones_like(span), np.cos(alpha)*span])
-ax.plot( guideline[0,:], guideline[1,:], guideline[2,:], 'k--', alpha=alpha_val)
-guideline = np.array([np.cos(beta)*np.sin(alpha)*(span), np.sin(beta)*np.sin(alpha)*(span), np.zeros_like(span)])
-ax.plot( guideline[0,:], guideline[1,:], guideline[2,:], 'k--', alpha=alpha_val)
-
-tmpvec = np.array([ np.cos(beta), np.sin(beta), 0.])
-guideline = np.array([z0 * s + psi * (1.-s) for s in span] )
-for i,p in enumerate(guideline):
-  guideline[i] = 0.45*p/norm(p)
-ax.plot( guideline[:,0], guideline[:,1], guideline[:,2], 'k')
-tmpvec2 = np.array([ 1., 0., 0.])
-guideline = np.array([tmpvec * s + tmpvec2 * (1.-s) for s in span] )
-for i,p in enumerate(guideline):
-  guideline[i] = 0.45*p/norm(p)
-ax.plot( guideline[:,0], guideline[:,1], guideline[:,2], 'k')
-
 #plot omega_dot
 omega_dot_arrow = Arrow3D([omega[0], omega[0]+omega_dot[0]], \
                           [omega[1], omega[1]+omega_dot[1]], \
@@ -192,16 +166,11 @@ ax.text(1.1*x0[0],1.1*x0[1],1.1*x0[2],r'$\mathbf{e}_1$', **text_options)
 ax.text(1.1*y0[0],1.1*y0[1],1.1*y0[2],r'$\mathbf{e}_2$', **text_options)
 ax.text(1.1*z0[0],1.1*z0[1],1.1*z0[2],r'$\mathbf{e}_3$', **text_options)
 ax.text(1.1*omega[0],1.1*omega[1],1.1*omega[2],r'$\omega$', **text_options)
-ax.text(1.1*psi[0],1.1*psi[1],1.1*psi[2],r'$\Psi$', **text_options)
 ax.text(omega[0] + omega_dot[0]*1.15, omega[1] + omega_dot[1]*1.15, omega[2]+omega_dot[2]*1.15, r'$\dot{\omega}$', **text_options)
 
 # add text for angles
 p = 0.45*(omega + z0)/2.0
 ax.text( p[0], p[1], p[2], r'$\theta$', **text_options)
-p = 0.4* (z0 + psi) / 2.0
-ax.text( p[0], p[1], p[2], r'$\beta$', **text_options)
-p = 0.6* (tmpvec + tmpvec2) / 2.0
-ax.text( p[0], p[1], p[2], r'$\gamma$', **text_options)
 
 # show figure
 ax.view_init(elev=25, azim=49)
