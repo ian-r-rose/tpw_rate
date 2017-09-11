aa020a149f88750ba334734ceab3e265cdca7f8a
Ian Rose <ian.r.rose@gmail.com>
2017-09-11 09:33:40 -0700

    Add tpw rate to figure.

--- figures/misfit.py
+++ figures/misfit.py
@@ -41,33 +41,46 @@ eigmean = np.mean(eig1 + eig2)/2.
 eigdiff = (eig1-eig2)/eigmean
 
 spin = data[:,1]
+spin_unwrap = np.unwrap(spin*np.pi*2./180.)*180./np.pi/2.
+spin_rate = interp.UnivariateSpline(time, spin_unwrap).derivative()(time)
 
 pos = np.where(np.abs(np.diff(spin)) >= 10.0)[0]
 time[pos] = np.nan
 spin[pos] = np.nan
 
 fig = plt.figure()
-ax = fig.add_subplot(211)
+ax = fig.add_subplot(311)
 
 ax.plot(time, eigdiff, label=r'$\Lambda_{21} = (\lambda_2-\lambda_1)/I_0$')
-plt.xlim(tmin, tmax)
-plt.ylim(0., 2.e-5)
+ax.set_xlim(tmin, tmax)
+ax.set_ylim(0., 2.e-5)
 ax.legend(loc='upper right')
 ax.xaxis.set_major_formatter(matplotlib.ticker.NullFormatter())
 ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(nbins=4, prune=None))
-plt.ylabel(r'Eigengap')
+ax.set_ylabel(r'Eigengap')
 
-ax = fig.add_subplot(212)
+ax = fig.add_subplot(312)
 
 ax.plot(np.array([]), np.array([]))
 ax.plot(time, spin, label=r'Spin axis')
 ax.plot(np.array([]), np.array([]))
 ax.plot(time, theta(time), label=r'$\theta$')
-plt.xlim(tmin, tmax)
-plt.ylim(0., 180.)
+ax.set_xlim(tmin, tmax)
+ax.set_ylim(0., 180.)
 ax.legend(loc='upper right')
+ax.xaxis.set_major_formatter(matplotlib.ticker.NullFormatter())
 ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(nbins=3, prune=None))
-plt.xlabel(r"Time (Gyr)")
-plt.ylabel(r"Angle ($^\circ$)")
+ax.set_ylabel(r"Angle ($^\circ$)")
+
+ax = fig.add_subplot(313)
+
+ax.plot(np.array([]), np.array([]))
+ax.plot(np.array([]), np.array([]))
+ax.plot(time, spin_rate/1000., label=r'Spin axis')
+ax.set_xlim(tmin, tmax)
+ax.set_xlabel(r"Time (Gyr)")
+ax.set_ylabel(r'TPW rate ($^\circ$/Myr)')
+
 
 plt.savefig("misfit.pdf", bbox_inches='tight')
+plt.show()
